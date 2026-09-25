4f096995706d1a6df6b9c3142a08d206a00ee6ab
Ezequiel <ealegremendoza@frba.utn.edu.ar>
2026-09-25 17:27:10 -0300

    fix(remote): remove missing C++ task_server node from launch

--- robotic_arm_ws/src/robotic_arm_remote/launch/remote_interface.launch.py
+++ robotic_arm_ws/src/robotic_arm_remote/launch/remote_interface.launch.py
@@ -1,7 +1,6 @@
 import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.conditions import UnlessCondition, IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
@@ -15,20 +14,7 @@ def generate_launch_description():
         default_value="True"
     )
 
-    use_python_arg = DeclareLaunchArgument(
-        "use_python",
-        default_value="False",
-    )
-
-    use_python = LaunchConfiguration("use_python")
     is_sim = LaunchConfiguration("is_sim")
-
-    task_server_node = Node(
-        package="robotic_arm_remote",
-        executable="task_server_node",
-        condition=UnlessCondition(use_python),
-        parameters=[{"use_sim_time": is_sim}]
-    )
 
     moveit_config = (
         MoveItConfigsBuilder("robotic_arm", package_name="robotic_arm_moveit")
@@ -44,17 +30,14 @@ def generate_launch_description():
         .to_moveit_configs()
     )
 
-    task_server_node_py = Node(
+    task_server_node = Node(
         package="robotic_arm_remote",
         executable="task_server",
-        condition=IfCondition(use_python),
         parameters=[moveit_config.to_dict(),
                     {"use_sim_time": is_sim}]
     )
 
     return LaunchDescription([
-        use_python_arg,
         is_sim_arg,
         task_server_node,
-        task_server_node_py,
     ])
